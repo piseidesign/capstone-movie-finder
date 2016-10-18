a489379b29f1a036b5603024b98cb035c76e9998
Pisei <limpisei@gmail.com>
2016-10-18 12:19:56 -0700

    cleanups

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
   $('#btn-go').click(function() {
-    var category = $('.category').val();
-    // var rating =
     var searchTitle = $('#movie-title').val();
-
     if (searchTitle.length) {
       getRequest(searchTitle);
     }
-
   });
 });
 // functions
@@ -24,13 +20,12 @@ function getRequest(searchTitle) {
 }
 
 function showResults(results) {
-  console.log(results);
-  if (results.Response === "False") {
-    $('#search-results h3').text(results.Error);
+  // console.log(results.Response);
+  if (results.Response == "False") {
+    $('#search-results').html("<h3 class='text-center text-placeholder'>" + results.Error + "</h3>");
   } else {
     var html = "";
-    html += '<div class="media"><div class="media-left"><img class="media-object img-thumbnail" src="' + results.Poster + '" alt="..."></div><div class="media-body"><h4 class="media-heading">' + results.Title + '</h4><ul class="list-inline"><li><strong>IMDb:</strong> ' + results.imdbRating + '</span></li><li>' + results.Year + '</li><li>' + results.Rated + '</li><li>' + results.Runtime + '</li></ul><p>' + results.Genre + '</p><p>' + results.Plot + '</p><p>' + results.Actors + '</p></div></div>';
+    html += '<div class="media"><div class="media-left"><img class="media-object img-thumbnail" src="' + results.Poster + '" alt="..."></div><div class="media-body"><h4 class="media-heading">' + results.Title + '</h4><ul class="list-inline"><li><strong>IMDb:</strong> ' + results.imdbRating + ' /10</span></li><li>' + results.Year + '</li><li>' + results.Rated + '</li><li>' + results.Runtime + '</li></ul><p>' + results.Genre + '</p><p>' + results.Plot + '</p><p>' + results.Actors + '</p></div></div>';
   }
-
   $('#search-results').html(html);
 }
